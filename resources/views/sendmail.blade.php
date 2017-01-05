954f1a091a080e56153781b2338aa1c1712c6490
Akashi_SN <btorntireinvynriy@gmail.com>
2017-01-05 20:43:26 +0900

    laravel update

--- resources/views/sendmail.blade.php
+++ resources/views/sendmail.blade.php
@@ -1,17 +1,30 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <title>Admin</title>
-  <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-  <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+	<meta charset="UTF-8">
+	<title>Admin</title>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
 </head>
 <body>
 <div class="container">
-  <div class="page-header" id="banner">
+	<div class="page-header" id="banner">
 		<div class="row">
-		  <h1>メール送信</h1>
-		  <br>
+			<h1>メール送信</h1>
+			<br>
+			<div class="form-group">
+				<label for="inputEmail" class="col-lg-2 control-label">Email</label>
+				<div class="col-lg-10">
+					<input type="text" class="form-control" id="inputEmail" placeholder="Email">
+				</div>
+			</div>
+			<div class="form-group">
+        <div class="col-lg-10 col-lg-offset-2">
+          <button type="reset" class="btn btn-default">キャンセル</button>
+          <button type="submit" class="btn btn-primary">送信</button>
+        </div>
+      </div>
 		</div>
 	</div>
 </div>
