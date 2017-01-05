e550ca9ca48d55c6b4b9afa79869d7f39d58436d
Akashi_SN <btorntireinvynriy@gmail.com>
2017-01-05 20:38:11 +0900

    laravel update

--- resources/views/sendmail.blade.php
+++ resources/views/sendmail.blade.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-	<title>sendmail</title>
+  <meta charset="UTF-8">
+  <title>Admin</title>
+  <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+  <script type="text/javascript" src="/js/bootstrap.min.js"></script>
 </head>
 <body>
-test!
+<div class="container">
+  <div class="page-header" id="banner">
+		<div class="row">
+		  <h1>メール送信</h1>
+		  <br>
+		</div>
+	</div>
+</div>
 </body>
 </html>
