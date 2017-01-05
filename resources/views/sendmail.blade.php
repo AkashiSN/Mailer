7e15570505c9d0af0bebe646abdc30c3d9d94439
Akashi_SN <btorntireinvynriy@gmail.com>
2017-01-05 20:50:29 +0900

    laravel update

--- resources/views/sendmail.blade.php
+++ resources/views/sendmail.blade.php
@@ -1,32 +1,69 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<title>Admin</title>
-	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
-</head>
-<body>
-<div class="container">
-	<div class="page-header" id="banner">
-		<div class="row">
-			<h1>メール送信</h1>
-			<br>
-			<div class="form-group">
-				<label for="inputEmail" class="col-lg-2 control-label">Email</label>
-				<div class="col-lg-10">
-					<input type="text" class="form-control" id="inputEmail" placeholder="Email">
-				</div>
-			</div>
-			<div class="form-group">
-        <div class="col-lg-10 col-lg-offset-2">
-          <button type="reset" class="btn btn-default">キャンセル</button>
-          <button type="submit" class="btn btn-primary">送信</button>
+  <head>
+    <meta charset="UTF-8">
+    <title>Admin
+    </title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <script type="text/javascript" src="/js/jquery-3.1.1.min.js">
+    </script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="page-header" id="banner">
+        <div class="row">
+          <h1>メール送信
+          </h1>
+          <br>
         </div>
       </div>
-		</div>
-	</div>
-</div>
-</body>
+      <div class="col-lg-6">
+        <div class="well bs-component">
+          <form class="form-horizontal">
+            <fieldset>
+              <legend>メール送信
+              </legend>
+              <div class="form-group">
+                <label for="inputSubject" class="col-lg-2 control-label">Subject
+                </label>
+                <div class="col-lg-10">
+                  <input type="text" class="form-control" id="inputSubject" placeholder="Subject">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputEmail" class="col-lg-2 control-label">To:
+                </label>
+                <div class="col-lg-10">
+                  <input type="text" class="form-control" id="inputEmail" placeholder="To:">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputEmail" class="col-lg-2 control-label">From:
+                </label>
+                <div class="col-lg-10">
+                  <input type="text" class="form-control" id="inputEmail" placeholder="From:">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="textArea" class="col-lg-2 control-label">Body
+                </label>
+                <div class="col-lg-10">
+                  <textarea class="form-control" rows="100" id="textArea">
+                  </textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-lg-10 col-lg-offset-2">
+                  <button type="reset" class="btn btn-default">キャンセル
+                  </button>
+                  <button type="submit" class="btn btn-primary">送信
+                  </button>
+                </div>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
