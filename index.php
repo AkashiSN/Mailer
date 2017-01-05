2b17a4c67f3bec40732753c73b330bab15005f36
Akashi_SN <btorntireinvynriy@gmail.com>
2017-01-05 19:29:10 +0900

    update

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php 
 	require 'vendor/autoload.php';
 
-	$sendgrid = new SendGrid(getenv('SENDGRID_USERNAME'), getenv('SENDGRID_PASSWORD'));
-	$email = new SendGrid\Email();
-	$email->addTo('btorntireinvynriy@gmail.com')->
-	    setFrom('from@example.com')->
-	    setSubject('件名')->
-	    setText('こんにちは！');
+	$request_body = json_decode('{
+	  "personalizations": [
+	    {
+	      "to": [
+	        {
+	          "email": "btorntireinvynriy@gmail.com"
+	        }
+	      ],
+	      "subject": "Hello World from the SendGrid PHP Library!"
+	    }
+	  ],
+	  "from": {
+	    "email": "test@example.com"
+	  },
+	  "content": [
+	    {
+	      "type": "text/plain",
+	      "value": "Hello, Email!"
+	    }
+	  ]
+	}');
 
-	$sendgrid->send($email);
+	$apiKey = getenv('SENDGRID_API_KEY');
+	$sg = new \SendGrid($apiKey);
+
+	$response = $sg->client->mail()->send()->post($request_body);
+	echo $response->statusCode();
+	echo $response->body();
+	echo $response->headers();
 ?>
