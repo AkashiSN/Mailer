6609ee3dfaaadb6ad63a1cb46fd53a669fc8ed11
Akashi_SN <btorntireinvynriy@gmail.com>
2017-01-09 14:09:17 +0900

    update

--- send.php
+++ send.php
@@ -1,32 +1,21 @@
 <?php
-  require '../vendor/autoload.php';
+  require 'vendor/autoload.php';
 
-  $request_body = json_decode('{
-    "personalizations": [
-      {
-        "to": [
-          {
-            "email": "'.$_POST["To"].'"
-          }
-        ],
-        "subject": "'.$_POST["From"].'"
-      }
-    ],
-    "from": {
-      "email": "'.$_POST["Subject"].'"
-    },
-    "content": [
-      {
-        "type": "text/plain",
-        "value": "'.$_POST["Body"].'"
-      }
-    ]
-  }');
+  $to = $_POST['To'];
+  $subject = $_POST['Subject'];
+  $from = $_POST['From'];
+  $body = $_POST['Body'];
 
-  $apiKey = getenv('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');
+  $request_body['personalizations'][0]['to'][0]['email'] = $to;
+  $request_body['personalizations'][0]['subject'] = $subject;
+  $request_body['from']['email'] = $from;
+  $request_body['content'][0]['type'] = 'text/plain';
+  $request_body['content'][0]['value'] = $body;
+
+  $apiKey = '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';
   $sg = new \SendGrid($apiKey);
 
   $response = $sg->client->mail()->send()->post($request_body);
-  echo $response->statusCode();
-  echo $response->body();
-  echo $response->headers();
+  header("HTTP/1.1 301 Moved Permanently");
+  header("Location: /index.php");
+  exit();
