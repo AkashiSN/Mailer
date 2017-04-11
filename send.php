dd1ac82ca5132a909b4af1007f1c46e25834e9a2
Akashi_SN <btorntireinvynriy@gmail.com>
2017-04-11 11:54:23 +0900

    Yamlライブラリ追加

--- send.php
+++ send.php
@@ -1,35 +1,44 @@
 <?php
   require 'vendor/autoload.php';
+  use Symfony\Component\Yaml\Yaml;
   session_start();
 
-  for($i = 0; $i < sizeof($_POST['Destination']); $i++){
-    $DestinationName = $_POST['Destination-Name'][$i];
-    $Destination = $_POST['Destination'][$i];
-    $request_body = json_decode("{
-      \"personalizations\": [
-        {
-          \"to\": [
-            {
-              \"name\": \"$DestinationName\",
-              \"email\": \"$Destination\"
-            }
-          ],
-          \"subject\": \"${_POST['Subject']}\"
-        }
-      ],
-      \"from\": {
-        \"name\": \"${_POST['From-Name']}\",
-        \"email\": \"${_POST['From']}\"
-      },
-      \"content\": [
-        {
-          \"type\": \"${_POST['MIME']}\",
-          \"value\": \"${_POST['Body']}\"
-        }
-      ]
-    }");
-  }   
-
+  if(isset($_FILES['file']['tmp_name'])){
+    echo $_FILES['file']['tmp_name'];
+    $yaml = Yaml::parse(file_get_contents($_FILES['file']['tmp_name']));
+    print_r($yaml);
+  }
+  else{
+    for($i = 0; $i < sizeof($_POST['Destination']); $i++){
+      $DestinationName = $_POST['Destination-Name'][$i];
+      $Destination = $_POST['Destination'][$i];
+      $request_body = json_decode("{
+        \"personalizations\": [
+          {
+            \"to\": [
+              {
+                \"name\": \"$DestinationName\",
+                \"email\": \"$Destination\"
+              }
+            ],
+            \"subject\": \"${_POST['Subject']}\"
+          }
+        ],
+        \"from\": {
+          \"name\": \"${_POST['From-Name']}\",
+          \"email\": \"${_POST['From']}\"
+        },
+        \"content\": [
+          {
+            \"type\": \"${_POST['MIME']}\",
+            \"value\": \"${_POST['Body']}\"
+          }
+        ]
+      }");
+    }
+    echo "text";
+  }
+/*
   $apiKey = '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';
   $sg = new \SendGrid($apiKey);
 
@@ -38,4 +47,4 @@
   $_SESSION['statusCode'] = $response->statusCode();
   
   header("HTTP/1.1 301 Moved Permanently");
-  header("Location: index.php");
+  header("Location: index.php");*/
